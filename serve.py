5f614001fd2687cee4f65d1b6e3804e89f296ace
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2022-01-20 15:41:58 +0200

    First commit.

---
+++ serve.py
@@ -0,0 +1,27 @@
+from pprint import pprint
+
+from feast import FeatureStore
+
+REPO_PATH = "./feature_repo"
+
+
+def main():
+    store = FeatureStore(repo_path=REPO_PATH)
+
+    feature_vector = store.get_online_features(
+        features=[
+            "driver_hourly_stats:conv_rate",
+            "driver_hourly_stats:acc_rate",
+            "driver_hourly_stats:avg_daily_trips",
+        ],
+        entity_rows=[
+            {"driver_id": 1004},
+            {"driver_id": 1005},
+        ],
+    ).to_dict()
+
+    pprint(feature_vector)
+
+
+if __name__ == "__main__":
+    main()
